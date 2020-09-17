3f86eae64e18057e2431f9684015409147ec921e
iwishiwasaneagle <jh.ewers@gmail.com>
2020-09-17 16:31:26 +0100

    Main

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from data_models.positional.waypoint import Waypoint, Waypoints
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-waypoints = Waypoints(Waypoint(5,0,0), Waypoint(10, 0, 0), Waypoint(10, 10, 0),Waypoint(0, 10, 0))
+waypoints = Waypoints([Waypoint(5,0), Waypoint(10, 0), Waypoint(10, 10),Waypoint(0, 10)])
 
 sim = simulation.simulation.simulation(waypoints)
 vehicle = sim.run()
+
+plt.figure(2)
+plt.plot(vehicle.data['pos']['y'], vehicle.data['pos']['x'], '-r')
+plt.show()
