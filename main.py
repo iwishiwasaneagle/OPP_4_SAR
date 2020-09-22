f6f630455468b27d5d911e57e8eba4a576142084
iwishiwasaneagle <jh.ewers@gmail.com>
2020-09-22 12:03:30 +0100

    Mian

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ from simulation.parameters import *
 from data_models.probability_map import ProbabilityMap
 from waypoint_generation.waypoint_factory import WaypointFactory, WaypointAlgorithmEnum
 
-prob_map_img = "waypoint_generation/prob_map_1.png"
+prob_map_img = "waypoint_generation/prob_map_2.png"
 prob_map = ProbabilityMap.fromPNG(prob_map_img)
 
-waypoints = WaypointFactory(WaypointAlgorithmEnum.LHC_GW_CONV_E, prob_map, Waypoint(100,0)).generate()
+
+waypoints = WaypointFactory(WaypointAlgorithmEnum.LHC_GW_CONV_E, prob_map, Waypoint(25,0)).generate()
 
 # waypoints = Waypoints([Waypoint(5,0), Waypoint(10, 0), Waypoint(10, 10),Waypoint(0, 10)])
 
@@ -23,8 +24,9 @@ vehicle = sim.run()
 
 plt.figure()
 
-plt.imshow(prob_map.img)
+img = prob_map.toIMG()
+plt.imshow(img)
 plt.plot(vehicle.data['pos']['x'], vehicle.data['pos']['y'], '-r')
-plt.xlim(0,n)
-plt.ylim(0,m)
+plt.xlim(0,img.size[0])
+plt.ylim(0,img.size[1])
 plt.show()
