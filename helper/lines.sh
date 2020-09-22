c156e3b7d5a6c30a7f8cb293b44567598fa02053
iwishiwasaneagle <jh.ewers@gmail.com>
2020-09-22 09:51:56 +0100

    Lines

--- helper/lines.sh
+++ helper/lines.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-LINES="$(cloc $PWD --exclude-dir=venv,img --quiet --csv | grep -i SUM)"
+LINES="$(cloc . --exclude-dir=venv,img --quiet --csv | grep -i SUM)"
 echo "$(date),$(echo $LINES | awk -F',' '{print $1","$3","$4","$5}')" >> helper/lines.csv
 anybadge -l Code -s " lines" -v $(echo $LINES | awk -F',' '{print $5}')  > img/lines.svg
