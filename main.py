655f42d97e6164ceaecaa5c1fa89cdaeb7c9c152
iwishiwasaneagle <jh.ewers@gmail.com>
2020-09-23 16:07:36 +0100

    change to end display

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ from simulation.parameters import *
 from data_models.probability_map import ProbabilityMap
 from waypoint_generation.waypoint_factory import WaypointFactory, WaypointAlgorithmEnum
 
-prob_map_img = "waypoint_generation/prob_map_2.png"
+prob_map_img = "waypoint_generation/prob_map_4_location_based.png"
 prob_map = ProbabilityMap.fromPNG(prob_map_img)
 
 
-waypoints = WaypointFactory(WaypointAlgorithmEnum.LHC_GW_CONV_E, prob_map, Waypoint(25,0)).generate()
+waypoints = WaypointFactory(WaypointAlgorithmEnum.LHC_GW_CONV_E, prob_map, Waypoint(25,-1)).generate()
 
 # waypoints = Waypoints([Waypoint(5,0), Waypoint(10, 0), Waypoint(10, 10),Waypoint(0, 10)])
 
