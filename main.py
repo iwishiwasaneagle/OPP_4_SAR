60fb702121efdd08ef9178ec0c770495271e0a67
iwishiwasaneagle <jh.ewers@gmail.com>
2020-09-24 16:05:35 +0100

    Testing

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
-import simulation.simulation
-from data_models.positional.waypoint import Waypoint, Waypoints
+import src.simulation.simulation as sim
+from src.data_models.positional.waypoint import Waypoint, Waypoints
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-from simulation.parameters import *
+from src.simulation.parameters import *
 
 
-from data_models.probability_map import ProbabilityMap
-from waypoint_generation.waypoint_factory import WaypointFactory, WaypointAlgorithmEnum
+from src.data_models.probability_map import ProbabilityMap
+from src.waypoint_generation.waypoint_factory import WaypointFactory, WaypointAlgorithmEnum
 
-prob_map_img = "waypoint_generation/prob_map_4_location_based.png"
+prob_map_img = "src/waypoint_generation/prob_map_3_multimodal.png"
 prob_map = ProbabilityMap.fromPNG(prob_map_img)
 
 
-waypoints = WaypointFactory(WaypointAlgorithmEnum.LHC_GW_CONV_E, prob_map, Waypoint(25,-1)).generate()
+waypoints = WaypointFactory(WaypointAlgorithmEnum.LHC_GW_CONV_E, prob_map, Waypoint(25,25), threaded=True).generate()
 
-# waypoints = Waypoints([Waypoint(5,0), Waypoint(10, 0), Waypoint(10, 10),Waypoint(0, 10)])
-
-
+#waypoints = Waypoints([Waypoint(5,0), Waypoint(10, 0), Waypoint(10, 10),Waypoint(0, 10)])
 
 
-sim = simulation.simulation.simulation(waypoints)
+
+
+sim = sim.simulation(waypoints)
 vehicle = sim.run()
 
 plt.figure()
