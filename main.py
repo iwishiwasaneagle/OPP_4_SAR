6b78fa5dbfb51779c13a5ed16b57fa37d974cc47
iwishiwasaneagle <jh.ewers@gmail.com>
2020-09-29 15:06:56 +0100

    minor edits to use modified lawnmower

--- main.py
+++ main.py
@@ -8,22 +8,18 @@ from src.simulation.parameters import *
 from src.data_models.probability_map import ProbabilityMap
 from src.waypoint_generation.waypoint_factory import WaypointFactory, WaypointAlgorithmEnum
 
-prob_map_img = "src/waypoint_generation/prob_map_3_multimodal.png"
+prob_map_img = "src/waypoint_generation/prob_map_5_multimodal_small.png"
 prob_map = ProbabilityMap.fromPNG(prob_map_img)
 
 
-waypoints = WaypointFactory(WaypointAlgorithmEnum.LHC_GW_CONV_E, prob_map, Waypoint(25,25), threaded=True).generate()
-
+#waypoints = WaypointFactory(WaypointAlgorithmEnum.LHC_GW_CONV_E, prob_map, Waypoint(25,-1), threaded=True).generate()
 #waypoints = Waypoints([Waypoint(5,0), Waypoint(10, 0), Waypoint(10, 10),Waypoint(0, 10)])
+waypoints = WaypointFactory(WaypointAlgorithmEnum.MODIFIED_LAWNMOWER, prob_map, threaded = False, animate=False).generate() 
 
-
-
-
-sim = sim.simulation(waypoints)
+sim = sim.simulation(waypoints, animate=False)
 vehicle = sim.run()
 
 plt.figure()
-
 img = prob_map.toIMG()
 plt.imshow(img)
 plt.plot(vehicle.data['pos']['x'], vehicle.data['pos']['y'], '-r')
