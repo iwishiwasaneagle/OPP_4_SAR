1622368c0660f5d0aec7ee189f2fcb86dd9bfdd7
iwishiwasaneagle <jh.ewers@gmail.com>
2020-10-05 10:45:48 +0100

    pre-push total lines of code tracker

---
+++ helper/stats.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Copy/symlink this into .git/hooks as either "pre-commit" or "pre-push"
+echo "🪝 Pre-push hook: auto update stats.csv"
+
+CWD=$(git rev-parse --show-toplevel)
+echo "$(date),$(cloc $PWD --exclude-dir=venv,img --csv | grep SUM | awk -F',' '{print $1","$3","$4","$5}')" >> $CWD/helper/stats.csv
+
+git commit $CWD/helper/stats.csv -m "Auto: updated stats.csv"
+echo "✅ Pre-push hook completed"
