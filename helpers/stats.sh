3a6d8b8b7f4952f53ffd05a522e8e84a8a9fdcd1
iwishiwasaneagle <jh.ewers@gmail.com>
2020-10-24 17:16:04 +0100

    Move

--- helpers/stats.sh
+++ helpers/stats.sh
@@ -3,7 +3,7 @@
 echo "🪝 Pre-push hook: auto update stats.csv"
 
 CWD=$(git rev-parse --show-toplevel)
-echo "$(date),$(cloc $PWD --exclude-dir=venv,img --csv | grep SUM | awk -F',' '{print $1","$3","$4","$5}')" >> $CWD/helper/stats.csv
+echo "$(date),$(cloc $PWD --exclude-dir=venv,img --csv | grep SUM | awk -F',' '{print $1","$3","$4","$5}')" >> $CWD/helpers/stats.csv
 
-git commit $CWD/helper/stats.csv -m "Auto: updated stats.csv"
+git commit $CWD/helpers/stats.csv -m "Auto: updated stats.csv"
 echo "✅ Pre-push hook completed"
