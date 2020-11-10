b948724bdcef567493f77dc98b0b89e286daf7e7
Jan-Hendrik Ewers <jh.ewers@gmail.com>
2020-11-10 11:59:20 +0000

    Update lines.sh

--- helpers/lines.sh
+++ helpers/lines.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+cloc . --exclude-dir=venv,img --quiet --csv 
 LINES="$(cloc . --exclude-dir=venv,img --quiet --csv | grep -i SUM)"
 echo "LINES: $LINES"
 anybadge -l Code -s " lines" -v $(echo $LINES | awk -F',' '{print $5}') | tee img/lines.svg
