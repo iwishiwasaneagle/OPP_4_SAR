e1215e43186b09b3a349fa4e31f942d831901d3c
iwishiwasaneagle <jh.ewers@gmail.com>
2020-11-10 11:55:33 +0000

    Path

--- helpers/lines.sh
+++ helpers/lines.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 LINES="$(cloc . --exclude-dir=venv,img --quiet --csv | grep -i SUM)"
+echo "LINES: $LINES"
 anybadge -l Code -s " lines" -v $(echo $LINES | awk -F',' '{print $5}') | tee img/lines.svg
