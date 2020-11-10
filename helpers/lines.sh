7124e1e29c774c48d7cd9be309bfab7defa678da
iwishiwasaneagle <jh.ewers@gmail.com>
2020-11-10 13:36:35 +0000

    No need for cloc

--- helpers/lines.sh
+++ helpers/lines.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-LINES="$(cloc . --exclude-dir=venv,img --quiet --csv | grep -i SUM)"
-echo "LINES: $LINES"
-anybadge -l Code -s " lines" -v $(echo $LINES | awk -F',' '{print $5}') | tee img/lines.svg
+LINES="$(tail -n1 helpers/stats.csv | awk -F',' '{print $5}')"
+anybadge -l Code -s " lines" -v $LINES | tee img/lines.svg
