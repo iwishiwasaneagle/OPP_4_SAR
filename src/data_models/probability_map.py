bb94083810464bc5630f659477bd6c0e18cc395a
iwishiwasaneagle <jh.ewers@gmail.com>
2020-11-24 14:21:27 +0000

    Re-write of pmap

--- src/data_models/probability_map.py
+++ src/data_models/probability_map.py
@@ -1,128 +1,59 @@
+import PIL
 import numpy as np
 from numpy.lib.utils import deprecate
 from src.simulation.parameters import *
-from typing import TypeVar, List, Tuple
-from PIL import Image, ImageOps
+from typing import TypeVar
+from PIL import Image
 from src.data_models.positional.waypoint import Waypoint, Waypoints
-import matplotlib.path as mpp
-import matplotlib.transforms as mpt
-
-import matplotlib.pyplot as plt
 
 from loguru import logger
 
 T = TypeVar('T', bound='ProbabilityMap')
 
-
 class ProbabilityMap:
-    def __init__(self, prob_map: list):
-        self.hq_prob_map = np.array(prob_map)/np.sum(prob_map)
-        self.lq_prob_map = self.hq_prob_map
-        
-        self._hq_shape = np.shape(prob_map) # Bake it, for performance
-        self._lq_shape = self._hq_shape
-        
-        assert(np.isclose(np.sum(self.hq_prob_map),1))
-        
-    @staticmethod
-    def fromPNG(img_path: str) -> T:
+    def __init__(self, prob_map: list) -> None:
+        self.prob_map = np.copy(prob_map)/np.sum(prob_map)
+    
+    @classmethod
+    def fromPNG(cls,img_path: str) -> T:
         img = Image.open(img_path)
-        prob =  ProbabilityMap(np.array(img)[:,:,0])
-        return prob
-
-    def toIMG(self,prob_map_hq=True) -> Image:
-        if prob_map_hq:
-            prob_map = self.hq_prob_map
+        arr = np.array(img)
+        if len(arr.shape)==3:
+            mask = np.ones((arr.shape[0],arr.shape[1],4))
+            mask[:,:,1:] = 0        
+            return cls((mask*arr)[:,:,0])
         else:
-            prob_map = self.lq_prob_map
-        img_arr = (
-                np.interp(np.array([[(f,f,f) for f in g] for g in prob_map]),[np.min(prob_map),np.max(prob_map)],[0,254]
-                    )).astype(np.uint8)
-        img = Image.fromarray(img_arr)
-        return img 
+            return cls(arr)
 
+    def toIMG(self) -> PIL.Image:
+        mask = np.ones((*self.shape,3))
+        arr = np.reshape(self.prob_map,(*self.shape,1))
+        arr = mask*arr
+        img_arr = (
+                np.interp(arr,[np.min(arr),np.max(arr)],[0,254])).astype(np.uint8)
+        return PIL.Image.fromarray(img_arr)
+    
+    @property
+    def shape(self) -> tuple:
+        return self.prob_map.shape
     @property
     def max(self) -> float:
         return np.max(self.prob_map)
     @property
-    def prob_map(self) -> list:
-        return self.lq_prob_map
-    @property
-    def shape(self) -> list:
-        return self.hq_shape
-    @property
-    def hq_shape(self) -> list:
-        return self._hq_shape
-    @property
-    def lq_shape(self) -> list:
-        return self._lq_shape
-    @lq_shape.setter
-    def lq_shape(self,val) -> None:
-        if isinstance(val, (tuple,list)) and len(val) == 2:
-            logger.debug(f"Resizing probability map to {val}")
-            self._lq_shape = val
-            tmp_prob_map = np.array(self.toIMG().resize(val))[:,:,0]
-            self.lq_prob_map = tmp_prob_map/np.sum(tmp_prob_map)
-        else:
-            raise Exception("Invalid value for lq_shape")
-    
-    def lq_to_hq_coords(self,*args):
-        if len(args) == 2:
-            x, y = args
-            return (
-                np.interp(x, (0,self.lq_shape[0]),(0,self.hq_shape[0])),
-                np.interp(y, (0,self.lq_shape[1]),(0,self.hq_shape[1]))
-                    )
-        elif len(args) == 1:
-            arg = args[0]
-            if isinstance(arg,(tuple,list)) and len(arg) == 2:
-                return self.lq_to_hq_coords(arg[0],arg[1])
-            elif isinstance(arg,Waypoint):
-                x,y = self.lq_to_hq_coords(arg.x,arg.y)
-                return Waypoint(x,y)
-            elif isinstance(arg,Waypoints):
-                return Waypoints([Waypoint(self.lq_to_hq_coords(f.x, f.y)) for f in arg])
-    
-        elif all([len(f) == 2 and isinstance(f, (tuple, list, Waypoint)) for f in args]):
-            return [self.lq_to_hq_coords(f) for f in args]
-        
-        raise Exception()
-    
-    def hq_to_lq_coords(self,*args):
-        if len(args) == 2:
-            x, y = args
-            return (
-                np.interp(x, (0,self.hq_shape[0]),(0,self.lq_shape[0])),
-                np.interp(y, (0,self.hq_shape[1]),(0,self.lq_shape[1]))
-                    )
-        elif len(args) == 1:
-            arg = args[0]
-            if isinstance(arg,(tuple,list,np.ndarray)) and len(arg) == 2:
-                return self.lq_to_hq_coords(arg[0],arg[1])
-            elif isinstance(arg,Waypoint):
-                x,y = self.lq_to_hq_coords(arg.x,arg.y)
-                return Waypoint(x,y)
-            elif isinstance(arg,Waypoints):
-                return Waypoints([Waypoint(self.lq_to_hq_coords(f.x, f.y)) for f in arg])
-    
-        elif all([len(f) == 2 and isinstance(f, (tuple, list, Waypoint)) for f in args]):
-            return [self.lq_to_hq_coords(f) for f in args]
-        
-        raise Exception(args)
+    def min(self) -> float:
+        return np.min(self.prob_map)
 
-    def place(self, n:int=1,prob_map_hq=True) -> Waypoints:
-        prob = self.hq_prob_map if prob_map_hq else self.lq_prob_map
-        
-        x,y = np.meshgrid(np.arange(0,prob.shape[0]),np.arange(0,prob.shape[1]))
+    def resampled(self, x,y):
+        img = Image.fromarray(self.prob_map).resize((x,y),Image.BOX)        
+        return ProbabilityMap(np.array(img))
+    
+    def place(self, n:int=1) -> Waypoints:               
+        x,y = np.meshgrid(np.arange(0,self.shape[1]),np.arange(0,self.shape[0]))
         x,y = x.flatten(),y.flatten()
         xy  = np.vstack((x,y)).T
-
         xy_indices = np.arange(len(xy))
-
-        choices = np.random.choice(xy_indices, n, p=prob.flatten())
-
+        choices = np.random.choice(xy_indices, n, p=self.prob_map.flatten())
         points = xy[choices]
-
         return Waypoints(points)
 
     def __getitem__(self, key):
@@ -130,15 +61,14 @@ class ProbabilityMap:
             return self.prob_map[key]
         elif isinstance(key, Waypoint):
             return self.__getitem__((key.x, key.y))
-        elif isinstance(key, (tuple, list)):
+        elif isinstance(key, (np.ndarray,tuple, list)):
             i,j = key
             try:
-                return self.prob_map[int(j),int(i)]
+                return self.prob_map[int(j),int(i)] # Images are stored line then column hence we use y x indexing 
             except IndexError:
                 return -np.iinfo(np.int16).max
-
         raise KeyError(f"Type {type(key)} is not valid")
-
+    
     def __setitem__(self, key, data):
         if isinstance(key, int):
             self.prob_map[key] = data
@@ -146,10 +76,9 @@ class ProbabilityMap:
             self.prob_map[key.x, key.y] = data
         elif isinstance(key, (tuple, list)):
             i,j = key
-            self.prob_map[int(j),int(i)] = data
+            self.prob_map[int(j),int(i)] = data # Images are stored line then column hence we use y x indexing
         else:
             raise KeyError(f"Type {type(key)} is not valid")
 
     def __len__(self):
-        return self.shape[0]         
-
+        return self.shape[0]*self.shape[1]
