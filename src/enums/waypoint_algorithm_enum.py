46e042baac31234272326de1a0e19cef883c0d7a
iwishiwasaneagle <jh.ewers@gmail.com>
2020-11-24 14:23:13 +0000

    Update to enums

--- src/enums/waypoint_algorithm_enum.py
+++ src/enums/waypoint_algorithm_enum.py
@@ -4,7 +4,7 @@ class WaypointAlgorithmEnum(Enum):
     LHC_GW_CONV = auto()
     PARALLEL_SWATHS = auto()
     MODIFIED_LAWNMOWER  = auto()
-    PABO = auto()
+    UNKNOWN = auto()
     PABO_FMINCON = auto()
     PABO_GA = auto()
     PABO_PARTICLESWARM = auto()
