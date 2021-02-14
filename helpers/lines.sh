54f0d63f2e75c929db157f557f62e3968976b1a8
Jan-Hendrik Ewers <jh.ewers@gmail.com>
2021-02-14 18:02:39 +0000

    Delete helpers directory

--- helpers/lines.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-LINES="$(tail -n1 helpers/stats.csv | awk -F',' '{print $5}')"
-anybadge -l Code -s " lines" -v $LINES | tee img/lines.svg
--- helpers/stats.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Copy/symlink this into .git/hooks as either "pre-commit" or "pre-push"
-echo "🪝 Pre-push hook: auto update stats.csv"
-
-CWD=$(git rev-parse --show-toplevel)
-echo "$(date),$(cloc $PWD --exclude-dir=venv,img --csv | grep SUM | awk -F',' '{print $1","$3","$4","$5}')" >> $CWD/helpers/stats.csv
-
-git commit $CWD/helpers/stats.csv -m "Auto: updated stats.csv"
-echo "✅ Pre-push hook completed"
